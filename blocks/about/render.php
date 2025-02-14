703ed2126636e932650d2a32b372d15af60ac3fc
Harm van de Kraats <147072196+treffendharm@users.noreply.github.com>
2025-02-14 15:50:25 +0100

    Block about

--- blocks/about/render.php
+++ blocks/about/render.php
@@ -8,20 +8,31 @@ $images = get_field('images');
 
 
 <section class="content-grid block block-about">
-    <div class="col-7 subgrid-with">
-        <div class="col-7 title">
-            <h2><?= $title ?></h2>
-        </div>
+    <div class="content subgrid">
         <div class="col-7 subtitle">
-            <p><?= $subtitle ?></p>
+            <p class="h5"><?= $subtitle ?></p>
         </div>
-        <div class="col-5 text-wrapper">
-            <div class="text">
-                <?= $text ?>
+        <div class="col-7 title">
+            <h2 class="h3"><?= $title ?></h2>
+        </div>
+        <div class="content text-wrapper subgrid">
+            <div class="col-5 text-wrapper">
+                <div class="text">
+                    <p> <?= $text ?></p>
+                </div>
+                <?php if ($button) : ?>
+                    <a href="<?= $button['url'] ?>" class="btn" target="<?= $button['target'] ?>"><?= $button['title'] ?></a>
+                <?php endif; ?>
             </div>
-            <?php if ($button) : ?>
-                <a href="<?= $button['url'] ?>" class="button" target="<?= $button['target'] ?>"><?= $button['title'] ?></a>
-            <?php endif; ?>
+            <div class="col-10 start-3 subgrid image-wrapper">
+                <?php foreach ($images as $image) : ?>
+                    <div class="image">
+                        <img src="<?= $image['url'] ?>" alt="<?= $image['alt'] ?>">
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </div>
     </div>
+
+    <!-- Place the images here inside a  -->
 </section>

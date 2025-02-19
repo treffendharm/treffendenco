b83f5c6a8dfe8e979d42241a7d4cdba28a98e5d6
Harm van de Kraats <147072196+treffendharm@users.noreply.github.com>
2025-02-19 16:00:47 +0100

    Allow the image row to have a video as well

--- blocks/image-row/render.php
+++ blocks/image-row/render.php
@@ -19,15 +19,25 @@ if (!empty($block)) {
 
 <section class="content-grid <?= esc_attr($className); ?> <?= $margin_top ? 'margin-top-large' : '' ?> <?= $margin_bottom ? 'margin-bottom-large' : '' ?>">
     <div class="content image-row-wrapper">
-
-        <!-- Acf galary field -->
         <?php
         if (get_field('images')):
-            foreach (get_field('images') as $image):
-                echo wp_get_img($image, 'full', false, ['class' => 'load-in']);
+            foreach (get_field('images') as $media):
+                // Prepare media data for the utility function
+                $media_data = [
+                    'video_image' => $media['video_image'] ?? false,
+                    'image' => $media['image'] ?? false,
+                    'youtube_file' => $media['youtube_file'] ?? false,
+                    'youtube_url' => $media['youtube_url'] ?? '',
+                    'video_file' => $media['video_file'] ?? false,
+                    'thumbnail' => $media['thumbnail'] ?? false,
+                    'always_muted' => $media['always_muted'] ?? false,
+                    'loop' => $media['loop_video'] ?? false,
+                ];
+
+                
+                the_video_image_output($media_data);
             endforeach;
         endif;
         ?>
-
     </div>
 </section>
